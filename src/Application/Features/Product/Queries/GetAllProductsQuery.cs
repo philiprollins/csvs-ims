da013d6bcde8f8c3a993615e94b04ce685eaaa7a
Philip Rollins <philipwaynerollins@outlook.com>
2025-08-31 09:14:37 -0700

    Moved GetProductBySku into it's own file

--- src/Application/Features/Product/Queries/GetAllProductsQuery.cs
+++ src/Application/Features/Product/Queries/GetAllProductsQuery.cs
@@ -19,29 +19,3 @@ public class GetAllProductsQueryHandler(PartsDbContext dbContext)
         return Result.Ok(products);
     }
 }
-
-public class GetProductBySkuQuery : IQuery<Result<ProductDetailReadModel?>>
-{
-    public string Sku { get; set; } = string.Empty;
-
-    public static Result<GetProductBySkuQuery> Create(string sku)
-    {
-        if (string.IsNullOrWhiteSpace(sku))
-            return Result.Fail<GetProductBySkuQuery>("sku", "SKU cannot be empty");
-
-        return Result.Ok(new GetProductBySkuQuery { Sku = sku });
-    }
-}
-
-public class GetProductBySkuQueryHandler(PartsDbContext dbContext)
-    : IQueryHandler<GetProductBySkuQuery, Result<ProductDetailReadModel?>>
-{
-    public async Task<Result<ProductDetailReadModel?>> HandleAsync(GetProductBySkuQuery query, CancellationToken cancellationToken = default)
-    {
-        var product = await dbContext.ProductDetails
-            .Include(p => p.PartTransactions.OrderByDescending(t => t.Timestamp))
-            .SingleOrDefaultAsync(p => p.Sku == query.Sku, cancellationToken);
-
-        return Result.Ok(product);
-    }
-}
---
+++ src/Application/Features/Product/Queries/GetProductBySku.cs
@@ -0,0 +1,32 @@
+using Application.Features.Product.Projections;
+using Library;
+using Library.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Product.Queries;
+
+public class GetProductBySkuQuery : IQuery<Result<ProductDetailReadModel?>>
+{
+    public string Sku { get; set; } = string.Empty;
+
+    public static Result<GetProductBySkuQuery> Create(string sku)
+    {
+        if (string.IsNullOrWhiteSpace(sku))
+            return Result.Fail<GetProductBySkuQuery>("sku", "SKU cannot be empty");
+
+        return Result.Ok(new GetProductBySkuQuery { Sku = sku });
+    }
+}
+
+public class GetProductBySkuQueryHandler(PartsDbContext dbContext)
+    : IQueryHandler<GetProductBySkuQuery, Result<ProductDetailReadModel?>>
+{
+    public async Task<Result<ProductDetailReadModel?>> HandleAsync(GetProductBySkuQuery query, CancellationToken cancellationToken = default)
+    {
+        var product = await dbContext.ProductDetails
+            .Include(p => p.PartTransactions.OrderByDescending(t => t.Timestamp))
+            .SingleOrDefaultAsync(p => p.Sku == query.Sku, cancellationToken);
+
+        return Result.Ok(product);
+    }
+}
