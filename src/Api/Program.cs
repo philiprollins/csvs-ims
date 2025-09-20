b38cd1b7804b1c58153369901fb39f00572083d3
Philip Rollins <philipwaynerollins@outlook.com>
2025-09-19 20:01:30 -0700

    Refactor Program.cs and update solution file to remove unused OpenAPI configuration and adjust Client project GUID

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,14 +1,8 @@
 using Application;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-// Configure database
 builder.Services.AddDbContext<PartsDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PartsDb")));
 
@@ -17,7 +11,6 @@ builder.Services.AddDbContext<Library.EventStoreDbContext>(options =>
 
 var app = builder.Build();
 
-// Ensure database is created and migrated
 using (var scope = app.Services.CreateScope())
 {
     var partsDb = scope.ServiceProvider.GetRequiredService<PartsDbContext>();
@@ -27,36 +20,7 @@ using (var scope = app.Services.CreateScope())
     await eventStoreDb.Database.MigrateAsync();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
